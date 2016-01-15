5a7c07ae356c62f33e7398483424e8c4aa4e9733
mabi <crust87@gmail.com>
2016-01-15 16:16:02 +0900

    add license

--- app/src/main/java/com/crust87/centercropvideoviewsample/LoadVideoTask.java
+++ app/src/main/java/com/crust87/centercropvideoviewsample/LoadVideoTask.java
@@ -1,3 +1,24 @@
+/*
+ * Android-CenterCropVideoView
+ * https://github.com/crust87/Android-FFmpegExecutor
+ *
+ * Mabi
+ * crust87@gmail.com
+ * last modify 2016-01-15
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.crust87.centercropvideoviewsample;
 
 import android.content.Context;
